266d38ad8eca5312173af1353542d5f936752b04
nelhoesp <nelhofabrizio@gmail.com>
2026-07-13 12:12:41 -0500

    feat: agrega vista de infoproductos creados

---
+++ src/features/infoproducts/services/infoproductService.js
@@ -0,0 +1,41 @@
+import apiClient from '@/services/apiClient';
+
+const cleanParams = (params) => {
+  return Object.fromEntries(
+    Object.entries(params).filter(([, value]) => {
+      return value !== '' && value !== null && value !== undefined;
+    })
+  );
+};
+
+export const infoproductService = {
+    getCreatedInfoproducts(params = {}) {
+        return apiClient.get(`/me/infoproducts/?origin=created`, {
+            params: cleanParams(params),
+        });
+    },
+
+    getCategories() {
+        return apiClient.get('/infoproducts/categories');
+    },
+
+    getLevels() {
+        return apiClient.get('/infoproducts/levels');
+    },
+
+    getCertificateTemplates() {
+        return apiClient.get('/infoproducts/certificate-templates');
+    },
+
+    getCourseCertificates() {
+        return apiClient.get('/me/infoproducts/certificates');
+    },
+
+    sendReviewRequest(infoproductId) {
+        return apiClient.post(`/infoproducts/${infoproductId}/send-review-request`);
+    },
+
+    deleteInfoproduct(infoproductId) {
+        return apiClient.delete(`/infoproducts/${infoproductId}`);
+    },
+};
