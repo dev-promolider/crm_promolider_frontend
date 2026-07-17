4587400e41b87e406298dfaa813872a694bc4476
nelhoesp <nelhofabrizio@gmail.com>
2026-07-17 11:00:36 -0500

    feat: agrega componente OrderCourse

--- src/features/infoproducts/services/course/courseService.js
+++ src/features/infoproducts/services/course/courseService.js
@@ -3,5 +3,25 @@ import apiClient from "@/services/apiClient";
 export const courseService = {
     getOrders(courseId) {
         return apiClient.get(`/course/${courseId}/orders`);
+    },
+
+    changeOrder(courseId, classes) {
+        return apiClient.patch(
+            "/course/change-order",
+            {
+                id: courseId,
+                order: JSON.stringify(classes),
+            }
+        );
+    },
+
+    changeOrderModules(courseId, modules) {
+        return apiClient.patch(
+            "/course/change-order-module",
+            {
+                id: courseId,
+                order: JSON.stringify(modules),
+            },
+        );
     }
 }
